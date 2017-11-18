871eac45aff677f555fad83d64c49d8035b342e9
EnosawaAyana <s17c1021ce@s.chibakoudai.jp>
2017-11-18 17:33:14 +0900

    test

---
+++ 0919.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void){
+int b;
+unsigned long int a,i;
+ unsigned long int x;
+ x=1;
+ scanf("%d",&i);
+for (a=1;a<=i;a++){
+   x=x*a;
+   }
+   printf("%d!=%d\n",i,x);
+return 0; }
